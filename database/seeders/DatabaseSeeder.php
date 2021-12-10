619717d5969550c5ccbce4f2640cc7f849cb0043
christian <christian@pleno.digital>
2021-12-09 22:37:01 -0500

    feat: add authentication system with password verification

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,36 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->setFKCheckOff();
+        $this->call([
+            UserSeeder::class,
+        ]);
+        $this->setFKCheckOn();
+    }
+
+    private function setFKCheckOff()
+    {
+        switch (DB::getDriverName()) {
+            case 'mysql':
+                DB::statement('SET FOREIGN_KEY_CHECKS=0');
+                break;
+            case 'sqlite':
+                DB::statement('PRAGMA foreign_keys = OFF');
+                break;
+            default:
+        }
+    }
+
+    private function setFKCheckOn()
+    {
+        switch (DB::getDriverName()) {
+            case 'mysql':
+                DB::statement('SET FOREIGN_KEY_CHECKS=1');
+                break;
+            case 'sqlite':
+                DB::statement('PRAGMA foreign_keys = ON');
+                break;
+            default:
+        }
     }
 }
