ed9f95d30eea15df9c119330c3d2c000f175070e
christian <christian@pleno.digital>
2021-12-20 19:35:20 -0500

    refactor: update authentication unit tests

--- app/Events/BanUnbanUserEvent.php
+++ app/Events/BanUnbanUserEvent.php
@@ -3,9 +3,7 @@
 namespace App\Events;
 
 use App\Models\User;
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
@@ -18,10 +16,5 @@ class BanUnbanUserEvent
     public function __construct(
         public User $user
     ) {
-    }
-
-    public function broadcastOn(): Channel|array
-    {
-        return new PrivateChannel('channel-name');
     }
 }
--- app/Http/Requests/UserActiveInactiveRequest.php
+++ app/Http/Requests/UserActiveInactiveRequest.php
@@ -9,22 +9,12 @@ use Illuminate\Validation\ValidationException;
 
 class UserActiveInactiveRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
+    public function authorize(): bool
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
+    public function rules(): array
     {
         return [
             'banned_until' => 'present|nullable|integer|between:5,3650', // 10 years
