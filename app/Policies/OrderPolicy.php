376d0156e1cfbefd81c106de461c333b634cadb7
christian <christian@pleno.digital>
2022-01-09 04:03:59 -0500

    feat: add segurity for the payment gateway controller

---
+++ app/Policies/OrderPolicy.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Order;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class OrderPolicy
+{
+    use HandlesAuthorization;
+
+    public function update(User $userAuth, Order $order): bool
+    {
+        if ($userAuth->hasPermissionTo('users_update_orders')) {
+            return true;
+        } elseif ($userAuth->hasPermissionTo('user_manage_own_order')) {
+            return $userAuth->id === $order->user->id;
+        }
+        return false;
+    }
+}
