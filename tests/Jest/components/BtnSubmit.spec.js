58ac98de400311225fd237d63b6f99e4e4aa5aba
christian <christian@pleno.digital>
2022-02-01 13:33:18 -0500

    fix: add timeout in axios to display modal in component unit test

--- tests/Jest/components/BtnSubmit.spec.js
+++ tests/Jest/components/BtnSubmit.spec.js
@@ -1,11 +1,14 @@
 import BtnSubmit from '../../../resources/js/components/BtnSubmit.vue'
 import { mount } from '@vue/test-utils'
+import axios from 'axios'
 
+jest.mock('axios')
 window.scrollTo = jest.fn()
 Object.defineProperty(window, 'location', {
   writable: true,
   value: { reload: jest.fn() }
 })
+axios.post.mockResolvedValue({ data: { status: 200 } })
 
 describe('button sumit ajax wait', () => {
   const wrapper = mount(BtnSubmit, {
@@ -13,11 +16,9 @@ describe('button sumit ajax wait', () => {
       action: 'fakeLink'
     }
   })
-  it('disable button when clicked and request is ajax', (done) => {
+  it('disable button when clicked and request is ajax', () => {
     wrapper.find('button').trigger('click')
-    setTimeout(() => {
-      done()
-    })
+    expect(wrapper.vm.btn.innerText).toContain('Wait...')
   })
 })
 
